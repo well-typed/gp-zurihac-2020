e16f5b4b933a808ab3d936f23b8d4292c054d93f
Andres Loeh <andres@well-typed.com>
2020-06-13 13:08:23 +0200

    Add initial contents.

--- GP.hs
+++ GP.hs
@@ -1,2 +1,11 @@
 module GP where
 
+import Data.Coerce
+import Data.Functor.Const
+import Data.Functor.Identity
+import Data.Kind
+import Data.Proxy
+import Data.Void
+import GHC.TypeLits
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
